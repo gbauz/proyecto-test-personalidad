bd864d9f1993ec228467604f6fe606f16ccbbd9e
Giovanni Bauz <92232987+gbauz@users.noreply.github.com>
2025-04-19 07:42:47 -0500

    estilos globales

--- my-page/src/Features/Settings/Settings.jsx
+++ my-page/src/Features/Settings/Settings.jsx
@@ -1,25 +1,16 @@
 const Settings = () => {
-    return (
-      <div className="w-[300px]">
-      <h2 className="font-inter text-[18px] font-normal leading-[120%] text-[#766D6D]">
-      Crackle is more than just a sound;
-      <span className="text-[#000000] font-bold">
-      it's inspired by the physics term for the fifth derivative of position over time, symbolizing precision and control.
-      </span>
-      <span className="text-[#766D6D] font-normal">
-      In digital advertising, where billions of ads are managed in real time, speed and accuracy are crucial.
-    </span>
-    </h2>
-    <h1 className="font-orbiter font-semibold text-[56px] leading-[100%] text-[#1C1515]">
-  Powerful Features to Boost Your <span className="text-[#EA4711]">Productivity</span>
-</h1>
-<h1 className="font-orbiter text-2xl text-[#1C1515]">Hola mundo con TASA Orbiter</h1>
+  return (
+    <div className="w-[300px]">
+      <h1>Powerful Features to Boost Your <span style={{ color: '#EA4711' }}>Productivity</span></h1>
 
-    </div>   
-    
-    
-    );
-  };
-  
-  export default Settings;
-  
+      <h2>Crackle is more than just a sound</h2>
+
+      <p>
+        It's inspired by the physics term for the fifth derivative of position over time,
+        symbolizing precision and control. In digital advertising, speed and accuracy are crucial.
+      </p>
+    </div>
+  );
+};
+
+export default Settings;
