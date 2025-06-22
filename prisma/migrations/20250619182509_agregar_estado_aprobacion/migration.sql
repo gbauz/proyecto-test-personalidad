45ac6cf5c5018a03e967b1fe755dc6bde8b99213
Jorge Vera Zambrano <125833333+JorgeVeraz23@users.noreply.github.com>
2025-06-21 21:41:04 -0500

    xd

--- prisma/migrations/20250619182509_agregar_estado_aprobacion/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `postulacion` ADD COLUMN `estadoAprobacion` ENUM('ACEPTADA', 'RECHAZADA', 'PENDIENTE') NOT NULL DEFAULT 'PENDIENTE';
--- prisma/migrations/20250619192709_add_ondelete_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `postulacion` DROP FOREIGN KEY `postulacion_ofertaId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `postulacion` ADD CONSTRAINT `postulacion_ofertaId_fkey` FOREIGN KEY (`ofertaId`) REFERENCES `oferta`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
