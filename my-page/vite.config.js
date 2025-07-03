ffeb523554d6ffe2f7148e40488246070e591fbb
Giovanni Bauz <92232987+gbauz@users.noreply.github.com>
2025-07-02 21:22:27 -0500

    eliminar yarn

--- my-page/vite.config.js
+++ my-page/vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './',
-   build: {
-    outDir: 'dist',  
+  root: './', // Asegúrate de que la raíz esté configurada correctamente
+  build: {
+    outDir: 'dist',  // Directorio donde se generarán los archivos de producción
+    rollupOptions: {
+      input: 'index.html',  // Asegúrate de que se esté buscando correctamente el archivo index.html
+    },
   },
   plugins: [
     react(),
